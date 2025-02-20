d04f020ae60fa9208f6fca79d12ff7c642008693
SMG011 <samg988@stemcollegiate.ca>
2025-02-20 09:00:17 -0700

    feb20.py

--- feb20.py
+++ feb20.py
@@ -1,2 +1,23 @@
 #Today I learned:
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called", person, "Who did something cool like", thing, "at the wonderful", place,  "where you'll find me", rhyme)
+#this is the first part of code I did
+food = input("Name a type of food: ")
+plant = input("Name a plant: ")
+cookingType = input("What is a way to cook something? ")
+burntFood = input("How do you describe burnt food? ")
+householdItem = input("Name something in your house: ")
 
+print()
+print("Tonight's dinner will be!:")
+
+print("For dinner we will be making", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
