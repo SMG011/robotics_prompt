36c07e796cea68c826e6d9967c6647e3ebf531ae
SMG011 <samg988@stemcollegiate.ca>
2025-02-21 10:07:41 -0700

    feb21.py

---
+++ feb21.py
@@ -0,0 +1,11 @@
+#Today I learned:
+myName = input("What's your name?: ")
+print ("Your Name is:" + myName)
+myFriends = input("What's your friends name?: ")
+print ("Your friends name is:" + myFriends)
+myFavouriteRestraunt = input("What's your favourite restraunt?: ")
+print ("Your favourite restraunt is:" + myFavouriteRestraunt)
+iHate = input("What do you hate?:")
+print ("You hate:"+ iHate)
+myFavouriteFood = input("What's your favourite food?: ")
+print ("Your favourite food is:" + myFavouriteFood)
