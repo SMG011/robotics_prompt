9d9832096a5f80d479c156bb813f2b2d57ce5ff3
SMG011 <samg988@stemcollegiate.ca>
2025-02-23 00:25:17 -0700

    feb21.py

--- feb21.py
+++ feb21.py
@@ -1,5 +1,4 @@
-#Today I learned:
-myName = input("What's your name?: ")
+#Today I learned:myName = input("What's your name?: ")
 print ("Your Name is:" + myName)
 myFriends = input("What's your friends name?: ")
 print ("Your friends name is:" + myFriends)
@@ -9,3 +8,6 @@ iHate = input("What do you hate?:")
 print ("You hate:"+ iHate)
 myFavouriteFood = input("What's your favourite food?: ")
 print ("Your favourite food is:" + myFavouriteFood)
+
+
+print(f"Hello {myName}! Your and your friend {myFriends} go to {myFavouriteRestraunt} again. You eat {myFavouriteFood} as you walk down the streets you see something you hate {iHate}")
