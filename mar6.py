51b6b1994d4e918adae4984d720947cb073f0ce3
SMG011 <samg988@stemcollegiate.ca>
2025-03-06 09:06:38 -0700

    Update mar6.py

--- mar6.py
+++ mar6.py
@@ -1,14 +1,34 @@
 #Today I learned...
-myName = input("What's your name?: ")
-print ("Your Name is:" + myName)
-myFriends = input("What's your friends name?: ")
-print ("Your friends name is:" + myFriends)
-myFavouriteRestraunt = input("What's your favourite restraunt?: ")
-print ("Your favourite restraunt is:" + myFavouriteRestraunt)
-iHate = input("What do you hate?:")
-print ("You hate:"+ iHate)
-myFavouriteFood = input("What's your favourite food?: ")
-print ("Your favourite food is:" + myFavouriteFood)
-
-
-print(f"Hello {myName}! You and your friend {myFriends} go to {myFavouriteRestraunt} again. You eat {myFavouriteFood} as you walk down the streets you see something you hate {iHate}!")
+print("What 'Avengers' Character are you?")
+print()
+print("Let's answer these questions and let's find out.")  
+print()
+print("Please answer these questions with a Y for Yes and a N for No.")
+print()
+youGreen = input ("Are you the color green?")
+if youGreen == "Y":
+  print("You must be the Hulk!")
+else:
+  print("Then you're not the Hulk")
+print()
+youHammer = input ("Do you carry a hammer?")
+if youHammer == "Y":
+  print("You must be Thor!")
+else:
+  print("Then you're not Thor")
+print()
+youSpy = input ("Are you a spy?")
+if youSpy == "Y":
+  print("You must be Black Widow!")
+else:
+  print("Then you're not Black Widow")
+print()
+mindControl = input ("Do you control minds?")
+if mindControl == "Y":
+  print("You must be Scarlet Witch!")
+else:
+  print("Then you're not Scarlet Witch")
+print()
+notOne = input ("Aren't you just a normal person?")
+if notOne == "Y":
+  print("I guess you are not like any Avenger")
