180cc80eb7912c9334223ccad99c02dcab380ad9
SMG011 <samg988@stemcollegiate.ca>
2025-03-11 13:52:22 -0600

    Update mar11.py

--- mar11.py
+++ mar11.py
@@ -1,1 +1,12 @@
-
+#Replit 6: Today I learned...
+print("Secure Login")
+userName = input("What is your username?")
+passWord = input("What is your password?")
+if userName == "Sanvi" and passWord == "BilluluRules":
+  print("Welcome, Sanvi! I hope you are doing well today!")
+elif useraNme == "Ambika" and passWord == "BookzRoole":
+  print("Hi Ambika! Your looking cool today!")
+elif userName == "Sam" and passWord == "Irulez!":
+  print("Hi Sam! You are look amazing!")
+else:
+  print("You do not have access. Now, Go away weirdo!")
