ff114210e4fa5d3e66935e18d6cce9ef12667339
SMG011 <samg988@stemcollegiate.ca>
2025-03-13 08:45:53 -0600

    Create mar13.py

---
+++ mar13.py
@@ -0,0 +1,26 @@
+#Today I learned....
+print ()
+ print ("Are you a superfan of 'Gilmore Girls' or a fake fan? ")
+print()
+print("Answer the following questions to find out.")
+School = input("Does someone go to chilton with blue eyes? ")
+if School == "yes":
+  print("Correct!")
+else:
+  print("Wrong!")
+WhoSchool = input("And who goes to chilton with blue eyes? ")
+if WhoSchool == "Rory":
+    print("You got it")
+else:
+    print("Nice but try again!")
+Glasses= input("Does someone wear glasses? ")
+if Glasses == "yes":
+  print("Correct!")
+else:
+  print("Wrong!")
+WhoGlasses = input("And who wears glasses? ")
+if WhoGlasses == "Lane":
+  print("You got it")
+else:
+  print("Nice but try again!")
+print ( "Thanks for playing!")
