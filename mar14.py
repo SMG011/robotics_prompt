515c3516224a52332880993548355d9e33a1b647
SMG011 <samg988@stemcollegiate.ca>
2025-03-14 21:57:29 -0600

    Update mar14.py

--- mar14.py
+++ mar14.py
@@ -1,1 +1,33 @@
+print ("Hi! Welcome to your daily affirmation generator!")
+name = input("What is your name?")
+
+
+if name == "Sanvi" or name == "sanvi":
+  DOW = input("What is the day of the week is it?")
+  if DOW == "Monday" or DOW == "monday":
+    print ("Today's going to be a great monday for you!", name)
+    if DOW = "Tuesday" or DOW = "tuesday"
+    print ("Isn't today a wonderful tuesday?", name)
+    if DOW = "Wednesday" or DOW = "wednesday"
+    print ("Happy middle of the week!", name)
+  if DOW = "Thursday" or DOW = "thursday"
+    print ("Almost to the weekend!", name)
+  if DOW = "Friday" or DOW = "friday"
+    print ("It's finally Friday, YAY!", name)
+
+elif name = "Ambika" or name = "ambika":
+  DOW = input("What is the day of the week is it?")
+  if DOW == "Monday" or DOW == "monday":
+    print ("Today's going to be a great monday for you!", name)
+  if DOW = "Tuesday" or DOW = "tuesday"
+    print ("You look awesome today!", name)
+  if DOW = "Wednesday" or DOW = "wednesday"
+    print ("You look so happy today!", name)
+  if DOW = "Thursday" or DOW = "thursday"
+    print ("You've been doing an awesome job!", name)
+  if DOW = "Friday" or DOW = "friday"
+    print ("YAY! We hit the end of the week!", name)
+  else:
+   print("I do not know your name, but I hope you are doing awesome today. Have a great day!")
+
 
